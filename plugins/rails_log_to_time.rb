2cc302a2327488ff5444649541ac56509791b15f
Norman Chen <s965202030@gmail.com>
2016-11-15 17:08:30 +0800

    Symptom: There are some logs missing, when they have no timestamp. Root Cause: When pare the log without timestamp, it will be rescue and return input. Input is not time format. Solution: We cached success parsed logs time. And increase it 1 millisecond as next log time when the log has no timestamp.

--- plugins/rails_log_to_time.rb
+++ plugins/rails_log_to_time.rb
@@ -3,9 +3,10 @@ require 'json'
 module Fluent
   class TextParser
     class RailsLogToTimeParser < Parser
-      Plugin.register_parser("rails_log_to_time", self)
+      @@cached_time = [Time.now.to_f, Time.now.strftime('%Y-%m-%dT%H:%M:%S.%N')]
+      Plugin.register_parser('rails_log_to_time', self)
 
-      config_param :time_format, :string, :default => nil # time_format is configurable
+      config_param :time_format, :string, default: nil # time_format is configurable
 
       def configure(conf)
         super
@@ -16,14 +17,26 @@ module Fluent
       # If this is the in_tail plugin, it would be a line. If this is for in_syslog,
       # it is a single syslog message.
       def parse(input)
-        begin
-          output = /(\d{4})-(\d{2})-(\d{2})( |T)*(\d{2}):(\d{2}):(\d{2}).(\d{3,})/.match(input).to_s
-          output = output.gsub(" ", "T")
+        output = /(\d{4})-(\d{2})-(\d{2})( |T)*(\d{2}):(\d{2}):(\d{2}).(\d{3,})/.match(input).to_s
+        output = output.tr(' ', 'T')
+        # Here is some workaroud to avoid log cannot be parsed.
+        # So we cached parsed time as time of log cannot be parsed.
+        if !output.nil? && output.strip != ''
+          # If log can be pased, yield time of log and cached it in @@cached_time
           time = @time_parser.parse(output)
+          @@cached_time = [time, output]
           yield time, output
-        rescue
-          yield input
+        else
+          # Increate cached time 1 millisecond
+          # and set it as current log time.
+          time = Time.parse @@cached_time[1]
+          tmp = time.to_f
+          tmp += 0.001
+          @@cached_time[1] = Time.at(tmp).strftime('%Y-%m-%dT%H:%M:%S.%N')
+          yield @@cached_time
         end
+      rescue
+        yield input
       end
     end
   end
